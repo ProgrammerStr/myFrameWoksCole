608f8afc4863ae9735ae2c459958a03adb3c1278
Gabriel de Padua <gabriel.eng16@gmail.com>
2017-03-07 16:02:33 -0300

    first files;

---
+++ Generic-Files/generic.js
@@ -0,0 +1,32 @@
+// E S T R U T U R A  - J A V A S C R I P T ( E N C A P S U L A D A )
+
+// funcao recebe os paramentos:
+// $ seletor jquery
+// window
+// document
+// myVar - Variavel do tipo objeto que contem as mipulações do DOM
+
+// 'use strict' - para uso rigoroso do javacript
+// init - inicia as funções separadas
+
+// C O D E  B Y - G A B R I E L  P A D U A  |T H & - C # D E - S T R @ N G E|
+(function($, win, doc, myVar){
+'use strict';
+
+myVar = {
+
+};
+
+myVar.init = function() {
+
+};
+
+
+$(doc).ready(function(){
+
+	myVar.init();
+
+});
+
+
+}(jQuery, window, document, 'Private'));
